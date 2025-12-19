7a13ee189fa3c5c6bf2fc245e19c1b4dedb20903
Driedoutjerky <itmlimjw@gmail.com>
2025-12-19 14:14:44 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 3.java
    주석 추가

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Main {
 	public static void main(String [] args) {
+		// int 로 입력받으면 너무 큰 숫자의 제곱은 안 된다네요~ 
+		
 		long inputNum = 0;
 		long count = 1; // 실행 횟수
 		long degree = 2; // Time Complexity
@@ -18,4 +20,5 @@ public class Main {
 		System.out.println(count);
 		System.out.println(degree);
 	}
+
 }
