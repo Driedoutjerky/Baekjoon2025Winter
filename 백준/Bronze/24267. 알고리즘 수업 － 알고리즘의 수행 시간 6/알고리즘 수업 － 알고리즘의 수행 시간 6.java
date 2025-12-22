89f348fe73897e68e2cf8ee151fb04e21ce6019d
Driedoutjerky <itmlimjw@gmail.com>
2025-12-22 12:22:11 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 176 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long inputNum = (long) Integer.parseInt(scanner.nextLine());
+        long count = 0;
+        long degree = 3;
+        long temp = 0;
+        
+        if (inputNum < 3) {
+            count = 0; 
+        } else {
+            for(int i=1;i<inputNum-1;i++) {
+                temp+=i;
+                count+=temp;
+            }
+        }
+        
+        System.out.println(count);
+        System.out.println(degree);
+    }
+    
+}
